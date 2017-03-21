d4b1efeea7b2b7a7d549da4598977d1923509586
Portal <portal@portalprima.hondaprima.local>
2017-03-21 16:07:29 +0700

    fix maintenance

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -27,15 +27,8 @@ class CheckForMaintenanceMode
      */
     public function handle($request, Closure $next)
     {
-<<<<<<< HEAD
-        $ip = array('127.0.0.1','192.168.10.203','192.168.55.31');
-        if ($this->app->isDownForMaintenance() && !in_array($this->request->getClientIp(), $ip)) {
-            // return response('Be right back! ' . $this->request->getClientIp() .  , 500);
-=======
-        // return response($request->ip(), 503);
         if ($this->app->isDownForMaintenance() && !in_array($request->ip(), ['127.0.0.1','192.168.10.203']))
         {
->>>>>>> 6d3d4a28aae5071a3f63e8b0b9ce8de0da767590
             throw new HttpException(503);
         }
 
