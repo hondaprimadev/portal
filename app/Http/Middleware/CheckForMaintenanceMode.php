6d3d4a28aae5071a3f63e8b0b9ce8de0da767590
tirtamulya <tmulya@gmail.com>
2017-03-21 14:46:18 +0700

    fix supplier

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -10,13 +10,13 @@ use Closure;
 
 class CheckForMaintenanceMode
 {
-    protected  $request;
+    protected  $requests;
     protected $app;
 
-    public function __construct(Application $app, Request $request)
+    public function __construct(Application $app, Request $requests)
     {
         $this->app= $app;
-        $this->request = $request;
+        $this->requests = $requests;
     }
     /**
      * Handle an incoming request.
@@ -27,9 +27,9 @@ class CheckForMaintenanceMode
      */
     public function handle($request, Closure $next)
     {
-        $ip = array('127.0.0.1','192.168.10.203');
-        if ($this->app->isDownForMaintenance() && !in_array($this->request->getClientIp(), $ip)) {
-            // return response('Be right back! ' . $this->request->getClientIp() .  , 500);
+        // return response($request->ip(), 503);
+        if ($this->app->isDownForMaintenance() && !in_array($request->ip(), []))
+        {
             throw new HttpException(503);
         }
 
